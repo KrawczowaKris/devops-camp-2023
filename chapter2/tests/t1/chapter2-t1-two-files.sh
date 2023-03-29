56ffb524ddce9139c9af5665deae343620fac599
KrawczowaKris <nukinanada@gmail.com>
2023-03-29 13:43:24 +0500

    changes according to comments

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-func() {
-	if [ -f $1 ] 
-	then cat  $1
-	else touch $1; chmod 700 $1; openssl rand -base64 8 > $1
-	fi
-}
+file_1=$1
+file_2=$2
 
-func $1
-func $2
+for FILE in $file_1, $file_2; do
+	if [ -f $FILE ]
+		then cat $FILE
+	else
+		openssl rand -base64 8 > $FILE;
+		chmod 700 $FILE
+	fi
+done
