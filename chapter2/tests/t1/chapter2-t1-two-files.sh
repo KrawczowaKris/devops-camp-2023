60e720633fe1f7b50e8119179bc511d57d465fe7
KrawczowaKris <nukinanada@gmail.com>
2023-03-30 15:20:26 +0500

    Correction of remarks

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-file_1=$1
-file_2=$2
+# The script takes two arguments with filenames.
+# If these files exist, it outputs their contents, otherwise it creates them.
 
-for FILE in $file_1, $file_2; do
-	if [ -f $FILE ]
-		then cat $FILE
-	else
-		openssl rand -base64 8 > $FILE;
-		chmod 700 $FILE
-	fi
+if [ $# -ne 2 ]
+  then echo "Please enter two arguments"
+  exit 1
+fi
+
+FILE_1=$1
+FILE_2=$2
+
+for FILE in $FILE_1 $FILE_2; do
+  if [ -f "$FILE" ]; then
+    cat "$FILE"
+    echo "--------------"
+  else
+    openssl rand -base64 8 > "$FILE";
+    chmod 700 "$FILE"
+  fi
 done
