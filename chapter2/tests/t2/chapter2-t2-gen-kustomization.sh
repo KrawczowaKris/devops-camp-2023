964b81bb870255b1096f20fed676d891dd1df50f
KrawczowaKris <KrawczowaKris>
2023-03-30 16:16:32 +0500

    Correction of remarks

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -1,36 +1,41 @@
-#
+#!/bin/bash
+
 # generate_repository -- takes a repository name and generates a repository
 #
 # usage: generate_repository <NAME_REPOSITORY>
-#
-generate_repository() { 
-	local name_repository=$1
+
+FOLDER_PATH="/home/nadya/github_repos/devops-camp-2023"
+
+generate_repository_kustomization_config() {
+  local repository_name=$1
+
+  ssh-keygen -t ed25519 -N "" -f /repos/ssh-keys/$repository_name-deploy-key.pem
 
 # kustomize repository
-cat << EOF >> ~/github_repos/devops-camp-2023/kustomization.yaml
-  - name: $name_repository
+cat << EOF >> /repos/kustomization.yaml
+  - name: $repository_name
     namespace: argo-cd
     options:
       labels:
         argocd.argoproj.io/secret-type: repository
     literals:
-      - name=$name_repository
-      - url=git@github.com:KrawczowaKris/$name_repository.git
+      - name=$repository_name
+      - url=git@github.com:KrawczowaKris/$repository_name.git
       - type=git
       - project=default
     files:
-      - sshPrivateKey=./ssh-keys/$name_repository-deploy-key.pem
+      - sshPrivateKey=/repos/ssh-keys/$repository_name-deploy-key.pem
 EOF
 }
 
 # checking the introduction of arguments
-if [ $# -eq 0 ]
-	then echo "Please enter arguments"
-	exit 1
+if [ $# -eq 0 ]; then
+  echo "No arguments passed. Please add repositories names to proceed."
+  exit 1
 fi
 
 # start of file kustomization.yaml
-cat << EOF > ~/github_repos/devops-camp-2023/kustomization.yaml
+cat << EOF > /repos/kustomization.yaml
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 
@@ -40,14 +45,11 @@ generatorOptions:
 secretGenerator:
 EOF
 
-names_repositories=$*
+REPOSITORY_NAMES=$*
 
-for name_repository in $names_repositories; do
-	# generate ssh keys
-	ssh-keygen -t ed25519 -N "" -f ~/github_repos/devops-camp-2023/ssh-keys/$name_repository-deploy-key.pem
-
-	generate_repository $name_repository
+for repository_name in $REPOSITORY_NAMES; do
+  generate_repository_kustomization_config $repository_name
 done
 
 # kustomize build repositories
-kustomize build ~/github_repos/devops-camp-2023 > ~/github_repos/devops-camp-2023/output.yaml
+kustomize build $FOLDER_PATH > /repos/output.yaml
