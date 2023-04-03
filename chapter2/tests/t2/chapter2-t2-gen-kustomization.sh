595f08eb38780aa4dce24337725e540c7af9572a
KrawczowaKris <KrawczowaKris>
2023-04-03 11:27:59 +0500

    Correction of remarks

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -1,55 +1,52 @@
 #!/bin/bash
 
+FOLDER_PATH=repos
+KUSTOMIZATION_PATH=$FOLDER_PATH/kustomization.yaml
+
 # generate_repository -- takes a repository name and generates a repository
 #
 # usage: generate_repository <NAME_REPOSITORY>
 
-FOLDER_PATH="/home/nadya/github_repos/devops-camp-2023"
-
 generate_repository_kustomization_config() {
-  local repository_name=$1
+  local repository_name="$1"
 
-  ssh-keygen -t ed25519 -N "" -f /repos/ssh-keys/$repository_name-deploy-key.pem
+  ssh-keygen -y -t ed25519 -N "" -f $FOLDER_PATH/"$repository_name"-deploy-key.pem -q
 
 # kustomize repository
-cat << EOF >> /repos/kustomization.yaml
-  - name: $repository_name
+  cat << EOF >> "$FOLDER_PATH"/kustomization.yaml
+  - name: "$repository_name"
     namespace: argo-cd
     options:
       labels:
         argocd.argoproj.io/secret-type: repository
     literals:
-      - name=$repository_name
-      - url=git@github.com:KrawczowaKris/$repository_name.git
+      - name="$repository_name"
+      - url=git@github.com:KrawczowaKris/"$repository_name".git
       - type=git
       - project=default
     files:
-      - sshPrivateKey=/repos/ssh-keys/$repository_name-deploy-key.pem
+      - sshPrivateKey=$repository_name-deploy-key.pem
 EOF
 }
 
 # checking the introduction of arguments
-if [ $# -eq 0 ]; then
+if [ "$#" -eq 0 ]; then
   echo "No arguments passed. Please add repositories names to proceed."
   exit 1
 fi
 
 # start of file kustomization.yaml
-cat << EOF > /repos/kustomization.yaml
+cat << EOF > $KUSTOMIZATION_PATH
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
-
 generatorOptions:
   disableNameSuffixHash: true
-
 secretGenerator:
 EOF
 
-REPOSITORY_NAMES=$*
-
-for repository_name in $REPOSITORY_NAMES; do
-  generate_repository_kustomization_config $repository_name
+for repository_name in $*; do
+  generate_repository_kustomization_config "$repository_name"
 done
 
 # kustomize build repositories
-kustomize build $FOLDER_PATH > /repos/output.yaml
+kustomize build repos
