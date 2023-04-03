9b09238a48259e4481fbd071aac3584d146897ce
KrawczowaKris <KrawczowaKris>
2023-04-03 16:13:43 +0500

    Completion t8

---
+++ chapter2/tests/t8/chapter2-l4-t8-get-tcp-listening-connections.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# When you launch the script it should initially list all tcp connections in the port
+# range 10000-10100. New found connections should be displayed in the console as soon
+# as they're identified.
+
+ports=()
+all_ports=()
+
+while true; do
+  ports=( $(ss --tcp sport ge 10000 and sport le 10100 | awk '{ print $4,$5,$6,$7 }') )
+  count_ports="${#ports[@]}"
+
+  k=4
+  while [[ "$k" -le "$count_ports" ]]; do
+    port="${ports[$k]}"
+    if ! [[ "${all_ports[@]}" =~ "${port}" ]]; then
+      echo "${ports[$k]}" "${ports[$k+1]}"
+      all_ports+="${port}"
+    fi
+    ((k++)) && ((k++))
+  done
+
+  if [[ "${#all_ports[@]}" -eq 0 ]]; then
+    echo "No TCP listening sockets found bound to ports in 10000-10100 range"
+  fi
+
+  sleep 5
+done
