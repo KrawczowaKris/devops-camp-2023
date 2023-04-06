5355ecfa51a3540ae89cb34fdda3984c55e8d083
KrawczowaKris <KrawczowaKris>
2023-04-06 15:12:12 +0500

    Correction

--- chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
+++ chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# Script should get a folder as an argument and return a list of unique extensions of all files 
+# Script should get a folder as an argument and return a list of unique extensions of all files
 # (including in the nested directories)
 
-folder=$1
-files=$(find $folder -type f)
+if [ "$#" -ne 1 ]; then
+  echo "Please enter one argument."
+  exit 1
+fi
+
+FOLDER="$1"
+files=$(find "${FOLDER}" -type f)
 extensions=( )
 
 for file in $files; do
   extension="${file##*.}"
-  if [[ "$file" == *.* && ! "${extensions[0]}" =~ $extension ]]; then
-    extensions+=$extension
-    echo $extension
+  if [[ "${file}" == *.* ]]; then
+    if [[ "${file}" == *"/."* && \
+      $(echo "${file##*/}" | tr -cd '.' | wc -m) -eq 1 ]]; then
+      continue
+    else
+      extensions+="${extension} "
+    fi
   fi
 done
+
+echo "${extensions[@]}" | sed 's/.$//; s/ /\n/g' | sort | uniq
