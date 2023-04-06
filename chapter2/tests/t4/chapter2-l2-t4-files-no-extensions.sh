273c1111fc41c94bfe45a93a603105903be6d92e
KrawczowaKris <KrawczowaKris>
2023-04-06 11:32:36 +0500

    Fix work with hidden files

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -3,10 +3,15 @@
 # Script should get a folder as an argument and return all files
 # (including in the nested directories) but without the extension
 
-PATH_TO_FOLDER=$1
+PATH_TO_FOLDER="$1"
 
-files=$(find $PATH_TO_FOLDER -type f)
+files=$(find "${PATH_TO_FOLDER}" -type f)
 
 for file in $files; do
-  echo "${file%%.*}";
+  if [[ "${file}" == *"/."* && \
+      $(echo "${file##*/}" | tr -cd '.' | wc -m) -eq 1 ]]; then
+    echo "${file}"
+    continue
+  fi
+  echo "${file%.*}";
 done
