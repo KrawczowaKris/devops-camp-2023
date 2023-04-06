9cfb6fc295dbfe47dc45e4987aacd59e8eadd94d
KrawczowaKris <KrawczowaKris>
2023-04-06 15:13:46 +0500

    Completion t9

---
+++ chapter2/tests/t9/chapter2-l4-t9-get-tcp-listening-connections-in-terminator.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# The script should output the ports immediately after the shell is restarted,
+# without restarting the script.
+
+nohup $0 &
+
+ports=()
+all_ports=()
+
+while true; do
+  ports=( $(ss --tcp sport ge 10000 and sport le 10100 | awk '{ print $4,$5,$6,$7 }') )
+  count_ports="${#ports[@]}"
+
+  k=4
+  while [[ "$k" -le "$count_ports" ]]; do
+    port="${ports[$k]}"
+    if ! [[ "${all_ports[@]}" =~ "${port}" ]]; then
+      echo "${ports[$k]}" "${ports[$k+1]}"
+      all_ports+="${port}"
+    fi
+    ((k++)) && ((k++))
+  done
+
+  if [[ "${#all_ports[@]}" -eq 0 ]]; then
+    echo "No TCP listening sockets found bound to ports in 10000-10100 range"
+  fi
+
+  sleep 5
+done
+
