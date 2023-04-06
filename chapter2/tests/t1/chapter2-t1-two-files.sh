bc0d0ea36119a3e002f663c8727010eb211ef3d6
KrawczowaKris <KrawczowaKris>
2023-04-06 13:57:27 +0500

    Start of correction

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -3,28 +3,35 @@
 # The script takes two arguments with filenames.
 # If these files exist, it outputs their contents, otherwise it creates them.
 
+# separator -- function prints separator between files
+#
+# usage: separator
+#
+separator() {
+  echo "--------------"
+}
+
 if [ "$#" -ne 2 ]
   then echo "Please enter two arguments"
   exit 1
 fi
 
-for FILE in "$1" "$2"; do
+for FILE in "$@"; do
   if [ -f "$FILE" ]; then
     test -r "$FILE"
     if [[ "$?" -eq 1 ]]; then
       echo "$FILE is not readable"
-      echo "--------------"
+      separator
     else
-      test -r "$FILE"
       echo "$FILE"
       cat "$FILE"
-      echo "--------------"
+      separator
     fi
   else
     test -w "$FILE"
     if [[ "$?" -eq 1 ]]; then
       echo "$FILE is not writable"
-      echo "--------------"
+      separator
     else
       openssl rand -base64 8 > "$FILE"
       chmod 700 "$FILE"
