6aa445248237f394912ca426e26d7ad2efee8087
KrawczowaKris <KrawczowaKris>
2023-04-07 17:42:02 +0500

    Correction of remarks

--- chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
+++ chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
@@ -12,15 +12,18 @@ FOLDER="$1"
 files=$(find "${FOLDER}" -type f)
 extensions=( )
 
-for file in $files; do
-  extension="${file##*.}"
-  if [[ "${file}" == *.* ]]; then
-    if [[ "${file}" == *"/."* && \
-      $(echo "${file##*/}" | tr -cd '.' | wc -m) -eq 1 ]]; then
+for file in ${files}; do
+  filename="${file##*/}"
+  # Check if there is a dot in the filename
+  if [[ "${filename}" == *.* ]]; then
+    # If the first symbol of the string is a dot, check how many dots are in the
+    # string. If one, then this file is not suitable
+    if [[ "${filename:0:1}" == "." && \
+      $(echo "${filename}" | tr -cd '.' | wc -m) -eq 1 ]]; then
       continue
-    else
-      extensions+="${extension} "
     fi
+    extension="${file##*.}"
+    extensions+="${extension} "
   fi
 done
 
