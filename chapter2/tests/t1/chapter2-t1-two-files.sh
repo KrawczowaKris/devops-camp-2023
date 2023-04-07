8a20913ad07db7b8a2def5b10dda81845b538dad
KrawczowaKris <KrawczowaKris>
2023-04-07 16:10:00 +0500

    Error output fixing

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -8,7 +8,25 @@
 # usage: separator
 #
 separator() {
-  echo "--------------"
+  echo -e "--------------"
+}
+
+folder_access_testing_flag=0
+
+# folder_access_testing -- function checks read access to a file or write access to a folder
+#
+# usage: folder_access_testing <FILENAME> <TEST_FLAG> <ERROR_TYPE>
+#
+folder_access_testing() {
+  folder_access_testing_flag=0
+  local test_flag="$2"
+
+  test "${test_flag}" "${FILE}"
+  if [[ "$?" -eq 1 ]]; then
+    echo "${FILE} is not $3"
+    separator
+    folder_access_testing_flag=1
+  fi
 }
 
 if [ "$#" -ne 2 ]
@@ -17,24 +35,20 @@ if [ "$#" -ne 2 ]
 fi
 
 for FILE in "$@"; do
-  if [ -f "$FILE" ]; then
-    test -r "$FILE"
-    if [[ "$?" -eq 1 ]]; then
-      echo "$FILE is not readable"
-      separator
-    else
-      echo "$FILE"
-      cat "$FILE"
-      separator
+  if [ -f "${FILE}" ]; then
+    folder_access_testing "${FILE}" -r "readable"
+    if [[ "${folder_access_testing_flag}" -eq 1 ]]; then
+      continue
     fi
+    echo "${FILE}"
+    cat "${FILE}"
+    separator
   else
-    test -w "$FILE"
-    if [[ "$?" -eq 1 ]]; then
-      echo "$FILE is not writable"
-      separator
+    if [[ -d "${FILE%/*}" ]]; then
+      folder_access_testing "${FILE%/*}" -w "writable"
     else
-      openssl rand -base64 8 > "$FILE"
-      chmod 700 "$FILE"
+      openssl rand -base64 8 > "${FILE}"
+      chmod 700 "${FILE}"
     fi
   fi
 done
