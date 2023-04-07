69b640927d4402e44ca7cd479f31797942076b39
KrawczowaKris <KrawczowaKris>
2023-04-07 19:38:30 +0500

    Correction of remarks

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -3,7 +3,9 @@
 # The script generates ssh-keys for repositories, then starts building
 # cli kustomize.
 
-REPOS_PATH="repos"
+REPOS_PATH="./repos"
+mkdir -p "${REPOS_PATH}"
+
 KUSTOMIZATION_PATH="${REPOS_PATH}"/kustomization.yaml
 
 # generate_repository -- takes a repository name and generates a repository
@@ -11,29 +13,29 @@ KUSTOMIZATION_PATH="${REPOS_PATH}"/kustomization.yaml
 # usage: generate_repository <NAME_REPOSITORY>
 
 generate_repository_kustomization_config() {
-  local repository_name=$1
+  local repository_name="${1}"
 
-  ssh-keygen -t ed25519 -N "" -f ./$REPOS_PATH/"$repository_name"-deploy-key.pem -q
+  ssh-keygen -t ed25519 -N "" -f "${REPOS_PATH}/${repository_name}-deploy-key.pem" -q <<< y > /dev/null 2>&1
 
   # kustomize repository
-  cat << EOF >> "$REPOS_PATH"/kustomization.yaml
-  - name: "$repository_name"
+  cat << EOF >> "${KUSTOMIZATION_PATH}"
+  - name: "${repository_name}"
     namespace: argo-cd
     options:
       labels:
         argocd.argoproj.io/secret-type: repository
     literals:
-      - name="$repository_name"
-      - url=git@github.com:KrawczowaKris/"$repository_name".git
+      - name="${repository_name}"
+      - url=git@github.com:KrawczowaKris/${repository_name}.git
       - type=git
       - project=default
     files:
-      - sshPrivateKey=./$REPOS_PATH/$repository_name-deploy-key.pem
+      - sshPrivateKey=${repository_name}-deploy-key.pem
 EOF
 }
 
 # checking the introduction of arguments
-if [ "$#" -eq 0 ]; then
+if [ "${#}" -eq 0 ]; then
   echo "No arguments passed. Please add repositories names to proceed."
   exit 1
 fi
@@ -47,8 +49,8 @@ generatorOptions:
 secretGenerator:
 EOF
 
-for repository_name in $*; do
-  generate_repository_kustomization_config "$repository_name"
+for repository_name in ${*}; do
+  generate_repository_kustomization_config "${repository_name}"
 done
 
-kustomize build repos
+kustomize build "${REPOS_PATH}"
