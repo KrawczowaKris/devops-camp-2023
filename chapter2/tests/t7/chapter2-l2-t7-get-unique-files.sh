99bd5ee5cf4142cfda9bfe6b4c802344fbd2f63e
KrawczowaKris <nukinanada@gmail.com>
2023-04-11 17:33:47 +0500

    Start of correction

--- chapter2/tests/t7/chapter2-l2-t7-get-unique-files.sh
+++ chapter2/tests/t7/chapter2-l2-t7-get-unique-files.sh
@@ -3,14 +3,22 @@
 # Script get a folder as an argument and return a list of all unique files
 # with extensions but without the absolute path
 
-folder=$1
-files=$(find $folder -type f)
-uniq_files=( )
+if [ "${#}" -ne 1 ]; then
+  echo "Invalid number of argument. Please enter exactly one argument."
+  exit
+fi
 
-for file in $files; do
+PATH_FOLDER="${1}"
+files=$(find "${PATH_FOLDER}" -type f)
+name_files=()
+
+for file in ${files[@]}; do
   name_file="${file##*/}"
-  if [[ ! "${uniq_files[0]}" =~ $name_file ]]; then
-    uniq_files+=$name_file
-    echo $name_file
-  fi
+  name_files+=("$name_file")
+#  if [[ ! "${uniq_files[0]}" =~ ${name_file} ]]; then
+#    uniq_files+=${name_file}
+#    echo $name_file
+#  fi
 done
+
+echo "${name_files[@]}" | tr ' ' '\n' | sort -u
