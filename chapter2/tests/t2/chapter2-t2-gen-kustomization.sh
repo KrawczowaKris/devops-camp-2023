e997fb79e4c48e14ad7b07fb7d43094d374fe457
KrawczowaKris <nukinanada@gmail.com>
2023-04-14 12:45:39 +0500

    Adding check repository name

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -4,11 +4,10 @@
 # cli kustomize.
 
 REPOS_PATH="./repos"
-mkdir -p "${REPOS_PATH}"
-
 KUSTOMIZATION_PATH="${REPOS_PATH}"/kustomization.yaml
 
-# generate_repository -- takes a repository name and generates a repository
+# generate_repository_kustomization_config -- takes a repository name
+# and generates a repository
 #
 # usage: generate_repository <NAME_REPOSITORY>
 
@@ -40,6 +39,8 @@ if [ "${#}" -eq 0 ]; then
   exit 1
 fi
 
+mkdir -p "${REPOS_PATH}"
+
 # start of file kustomization.yaml
 cat << EOF > "${KUSTOMIZATION_PATH}"
 apiVersion: kustomize.config.k8s.io/v1beta1
@@ -50,7 +51,11 @@ secretGenerator:
 EOF
 
 for repository_name in ${*}; do
-  generate_repository_kustomization_config "${repository_name}"
+  if [[ ! "${repository_name}" =~ ^[A-Za-z0-9.-_]+$ ]]; then
+    echo "${repository_name}" is invalid repository name
+  else
+    generate_repository_kustomization_config "${repository_name}"
+  fi
 done
 
 kustomize build "${REPOS_PATH}"
