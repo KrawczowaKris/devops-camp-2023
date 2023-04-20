59f4171862dcb73a6ba675ca966f598be2c22616
KrawczowaKris <KrawczowaKris>
2023-04-20 16:23:07 +0500

    Completion task

---
+++ chapter3/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create the necessary folders and files
+sudo cp -r $(pwd)/reports /var/www
+cd /tmp && mkcert localhost
+cd -
+
+# Setting permissions
+sudo chown -R nginx:nginx /tmp/localhost.pem
+sudo chown -R nginx:nginx /tmp/localhost-key.pem
+sudo chmod 777 /var/log
+
+# Launch nginx and php-fpm
+sudo fuser -k 443/tcp
+sudo /usr/sbin/php-fpm8.2 --php-ini "$(pwd)/php.ini" -y "$(pwd)/php-fpm.conf"
+sudo nginx -c "$(pwd)/nginx.conf"
+
+sleep 3
+
+# Sending requests
+curl https://localhost/reports/fast
+echo
+curl https://localhost/reports/slow
