2811b31e712ef9805e012aa259ceb6ec1cfdf517
KrawczowaKris <KrawczowaKris>
2023-05-02 12:38:10 +0500

    Fixing task

--- chapter2/tests/t8/chapter2-l4-t8-get-tcp-listening-connections.sh
+++ chapter2/tests/t8/chapter2-l4-t8-get-tcp-listening-connections.sh
@@ -4,28 +4,25 @@
 # range 10000-10100. New found connections should be displayed in the console as soon
 # as they're identified.
 
-START_PORT=10000
-END_PORT=10100
-TIME_OUT=5
-HEADER_LIST="Local Address:Port      Peer Address:Port"
+start_port=10000
+end_port=10100
+time_out=5
 all_ports=""
 
-while true; do
+while (true); do
   # Get the required ports
-  ports=$( ss -ltnpH "( sport ge ${START_PORT} && sport le ${END_PORT} )" )
+  ports=$(ss -ltnp "( sport ge "${start_port}" && sport le "${end_port}" )")
   # Looking for new ports
-  new_port=$( comm -13 <(echo "${all_ports}") <(echo "${ports}") )
+  new_port=$( comm -13 <(echo "${all_ports}") <(echo "${ports}") 2> /dev/null )
 
-  if [[ -z "${ports}" ]]; then
-    echo "No TCP listening sockets found bound to ports in ${START_PORT}-${END_PORT} range"
-  elif [[ ! -z "${new_port}" ]]; then
-    if [[ -z "${all_ports}" ]]; then
-      echo "${HEADER_LIST}"
-    fi
-    # Display the new port in the table
-    echo "${new_port}" | sed -E "s/[[:space:]]+/ /g" | cut -d' ' -f 4,5 | sed -E "s/ /\t\t/"
+  if [[ "${ports[@]}" == "State Recv-Q Send-Q Local Address:Port Peer Address:PortProcess" ]]; then
+    echo "No TCP listening sockets found bound to ports in ${start_port}-${end_port} range"
+  elif ! [[ -z "${new_port}" ]]; then
+    unset '$ports[1]'
+    # Display ports
+    echo "${ports}" | awk '{print $4,$5,$6,$7}'
   fi
 
   all_ports="${ports}"
-  sleep "${TIME_OUT}"
+  sleep "${time_out}";
 done
